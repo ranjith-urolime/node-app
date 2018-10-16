ea37ed8ece980d6c3fa47296fd1333d64b52de6c
Ranjith KR <ranjith.kumar@urolime.com>
2018-10-16 13:49:44 +0530

    Added deplyment script

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var request = require('supertest');
 var app = require('../index.js');
 describe('GET /', function() {
- it('respond with hello world', function(done) {
- //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('hello world', done);
+ it('respond with hey world', function(done) {
+ //navigate to root and check the the response is "hey world"
+ request(app).get('/').expect('hey world', done);
  });
 });
